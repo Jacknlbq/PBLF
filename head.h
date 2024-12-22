a2fa1d63021bdb23fce818cc1980d549f5e95d16
Jacknlbq <2979734778@qq.com>
2024-12-22 17:59:46 +0800

    Update head.h

--- head.h
+++ head.h
@@ -5,13 +5,13 @@
 #define Maxsize 30//名字长度上限
 #define Max_Igd 20//原料上限
 #define Max_table 50 //桌位
-
+#define upgrademoney 1000//升级金额
 
 #define Igd_filename "Igd.txt"
 #define User_filename "User.txt"
 #define Foods_filename "Foods.txt"
 #define Strategy_filename "Strategy.txt"
-
+#define Resturant_filename "Resturant.txt"
 
 
 //原料信息
@@ -25,6 +25,8 @@ typedef struct ingredient{
     char ID[Maxsize];
     char code[Maxsize];
     char grade;//"区分用户等级，享受不同折扣,"A","B"...
+    double surplus;//余额
+    double consumption;//消费总额
     struct user* next;
 }User;
 
@@ -34,11 +36,11 @@ typedef struct source{
     int source_amount;//原料数量
 }Source;
 
-//食物信息，名字，价格，用料头指针，用料总类数, 唯一标识ID，菜品描述。
+//食物信息，名字，价格，用料指针，用料类数,唯一标识Id。
 typedef struct info_foods{
-    int food_id;
     char food_name[Maxsize];
-    int price;
+    double price;
+    int food_id;
     char food_description[100];//菜品描述
      Source *source;//顺序表存用料
      int NUM_source;//用料种类数
@@ -53,25 +55,26 @@ typedef struct menu{
 //点餐顺序，采用顺序表，队列
 typedef struct queue{
     char  ID[Maxsize];
+    char status;//是否是会员
     int NUM_foods;
     int *foos_id;
 }Queue;
 
 //优惠策略,排序二叉树存储
 typedef struct pstrategy{
-    int origin;//原价
+    double origin;//原价
     double discont;
     struct pstrategy *left,*right;
 }Strategy;
 
 typedef struct res_info{
-    char res_name[Maxsize];//餐厅名，只需初始化
-    char res_owner[Maxsize];//餐厅所有者，只需初始化
-    int res_contactnum[18];//联系电话，只需初始化
-    char res_address[Maxsize];//餐厅地址，只需初始化
-    int res_table; //= Max_table 桌位数
+    char res_name[Maxsize];//餐厅名
+    char res_owner[Maxsize];//餐厅所有者
+    char res_contactnum[18];//联系电话
+    char res_address[Maxsize];//餐厅地址
     float res_turnover;//= 0.00 餐厅营业额
-    float res_profit;//= res_turnover*0.25 餐厅利润
+    float res_profit;//= res_turnover*5 餐厅利润
+    char manager_code[Maxsize];//管理员密码
 }Res_info;
 
 //用于访问各种数据
@@ -80,8 +83,12 @@ typedef struct Data{
     User *users;
     Menu *menu;
     Strategy *strategy;
+
     int head,tail;
-    Queue* list;//指向队列头尾
+    Queue* list;//点餐队列
+
+    Res_info *res;
+
 }Data;
 #ifndef WUYU_H
 #define WUYU_H
@@ -93,19 +100,56 @@ Ingredient* init_Igd();
 Menu *init_menu();
 User* init_user();
 Strategy *init_strategy();
+Data * init_Data();
+void init_ID(Menu *p);
 void search_insert(Strategy *root,Strategy *p);
+Res_info* init_res();
+
 
 void push_Igd(Ingredient *p);
 void push_Menu(Menu* p);
 void push_user(User *p);
 void push_strategy(Strategy* p);
+void push_res(Data *p);
+void putout(Data *p);
 
-Source *get_source();
+
+void insert_queue(Data *q);
+Queue *init_queue(Data *q);
+Menu *search_foods(int ID,Menu *p);
+void pop_queue(Data* q);
+Strategy *search_strategy(double origin,Strategy *p);
+int search_ingredient(Ingredient *p,Menu *q);
+void modify_ingredient(Ingredient *p,Menu *q);
+
+
+Source *get_source(int n);
 info_foods *get_info();
 void menu_insert(Menu *L);
 void menu_delete(Menu *L);
 void menu_update(Menu *L);
 void menu_comment(Menu *L);
 void menu_print(Menu *L);
+
+User* logIn(User*head);
+void local_user(User *local);
+void printInfo(User* temp);
+void recharge(User* temp);
+void changeCode(User *temp);
+void delete_user(User *head);
+void add_user(User *head);
+User* isUserExist(User*head, char id[]);
+User* create_user() ;
+
+void User_main(User *head);
+void order_system(Data *p);
+void manager_main(Data *p);
+int manager_logIn(Res_info *res);
+
+void insert_Igd(Ingredient *p);
+void delete_Igd(Ingredient *p);
+void show_Igd(Ingredient *p);
+void update_Igd(Ingredient *p);
+void modify_res(Res_info *p);
 
 #endif
